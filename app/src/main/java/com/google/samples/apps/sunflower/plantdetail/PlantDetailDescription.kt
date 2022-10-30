06a2706f0af22332b5cec1626cc4d0e1ba41d702
강동현 <dhkang@spidor.co.kr>
2022-10-30 18:04:00 +0900

    PlantDetailDescription에 PlantDetailViewModel 매개변수 추가
    Plant 내용을 모아 보여줄 PlantDetailContent 추가
    
    Preview에 변경사항 반영

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -20,14 +20,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
 private fun PlantName(name: String) {
@@ -44,14 +47,22 @@ private fun PlantName(name: String) {
 @Preview
 @Composable
 private fun PlantNamePreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
     MaterialTheme {
-        PlantName(name = "Apple")
+        PlantDetailContent(plant)
     }
 }
 
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
+    val plant by viewModel.plant.observeAsState()
+
+    plant?.run {
+        PlantDetailContent(this)
     }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    PlantName(plant.name)
 }
